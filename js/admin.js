327f1e162cfa3f3d867e7dc7cd11373c1b5689c2
juu98atlas-png <juu98.atlas@gmail.com>
2026-01-06 18:26:14 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,2 +1,14 @@
-// Acesso pelo comando /Analise com senha 0000
-// Aqui podemos criar funções futuras de atribuição de emblemas/cargos
+// Sala de admin bloqueada: /Analise
+const secretCode = "0000";
+
+function checkAdmin(command) {
+  if(command === "/Analise") {
+    const pass = prompt("Senha de admin:");
+    if(pass === secretCode) {
+      alert("Acesso liberado!");
+      // Aqui podemos abrir o painel de administração
+    } else {
+      alert("Senha incorreta!");
+    }
+  }
+}
