f25aa387f1ea7759ff566f457f603f8805945075
juu98atlas-png <juu98.atlas@gmail.com>
2026-01-07 17:58:37 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,17 @@
-// firebase.js
-// Configuração do Firebase
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Firebase Web Setup
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
+import { getDatabase, ref, set, get, child, update } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js";
 
-// Substitua pelos dados do seu projeto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB2Zc2WQRZZt-jkU1HYeOupfHpLc-mSygo",
+  apiKey: "SUA_API_KEY_AQUI",
   authDomain: "after-server.firebaseapp.com",
   projectId: "after-server",
   storageBucket: "after-server.appspot.com",
   messagingSenderId: "835893170150",
-  appId: "1:835893170150:web:b2edbe4cb4616e57489027",
-  measurementId: "G-Y19V9G8YFV"
+  appId: "1:835893170150:web:b2edbe4cb4616e57489027"
 };
 
-// Inicializa Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getDatabase(app);
