4dceabef04adf5f414414a737e538ca03ad7d25b
juu98atlas-png <juu98.atlas@gmail.com>
2026-01-07 16:40:34 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,23 @@
-<!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js";
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyB2Zc2WQRZZt-jkU1HYeOupfHpLc-mSygo",
-    authDomain: "after-server.firebaseapp.com",
-    projectId: "after-server",
-    storageBucket: "after-server.appspot.com",
-    messagingSenderId: "835893170150",
-    appId: "1:835893170150:web:b2edbe4cb4616e57489027"
-  };
+// CONFIG DO SEU PROJETO
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Zc2WQRZZt-jkU1HYeOupfHpLc-mSygo",
+  authDomain: "after-server.firebaseapp.com",
+  projectId: "after-server",
+  storageBucket: "after-server.appspot.com",
+  messagingSenderId: "835893170150",
+  appId: "1:835893170150:web:b2edbe4cb4616e57489027"
+};
 
-  firebase.initializeApp(firebaseConfig);
+// INICIALIZA
+const app = initializeApp(firebaseConfig);
 
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+// EXPORTA GLOBALMENTE
+window.auth = getAuth(app);
+window.db = getFirestore(app);
+
+console.log("🔥 Firebase conectado com sucesso");
